c2b939374f2df1323068cd4de7f4a2272f1cf27b
Matt Pritchard <matt@soliduslabs.com>
2020-06-11 23:24:34 -0400

    add spiral matrix solution

---
+++ src/medium/054_spiral_matrix.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+
+const spiralOrder = matrix => {
+  let results = [];
+
+  let rowBegin = 0;
+  let colBegin = 0;
+  let rowEnd = matrix.length - 1;
+  let colEnd = matrix[0].length - 1;
+
+  while (rowBegin <= rowEnd && colBegin <= colEnd) {
+    for (let i = colBegin; i <= colEnd; i++) {
+      results.push(matrix[rowBegin][i]);
+    }
+
+    rowBegin++;
+
+    for (let i = rowBegin; i <= rowEnd; i++) {
+      results.push(matrix[i][colEnd]);
+    }
+
+    colEnd--;
+
+    if (rowBegin <= rowEnd) {
+      for (let i = colEnd; i >= colBegin; i--) {
+        results.push(matrix[rowEnd][i]);
+      }
+    }
+
+    rowEnd--;
+
+    if (colBegin <= colEnd) {
+      for (let i = rowEnd; i >= rowBegin; i--) {
+        results.push(matrix[i][colBegin]);
+      }
+    }
+
+    colBegin++;
+  }
+
+  return results;
+};
